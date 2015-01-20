8e291254feb4ae0661acde5182cae744a895ff2e
DHOMD <DHO@dho.im>
2015-01-19 20:44:20 -0500

    vanir != github,... but really just better :p
    Change-Id: I3e1474c9f30ed2f8fb75aa5b4c70333312df9792

--- kernel_special_cases.sh
+++ kernel_special_cases.sh
@@ -75,7 +75,7 @@ case $1 in
         export kernelsource="kernel_samsung_klte"
     ;;
     m7*)
-        export remote="github"
+        export remote="vanir"
         export remoterevision="L5"
         export kernelsource="kernel_htc_m7"
     ;;
