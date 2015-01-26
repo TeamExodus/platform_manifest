e6a4c8a9828fffd70e56e642d2f5b96c1b067ace
DHOMD <DHO@dho.im>
2015-01-26 17:36:43 -0500

    Mothball a couple buils
    These are currently not working, but I would like to restore then eventually,...
    Leave them here for easy access by anyone up to the challange
    
    Change-Id: Ib4ae4d2375b7470b6a643e63fcd0db0a8891539e

---
+++ mothballs.sh
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<manifest>
+
+  <project path="device/lge/gee-common" name="device_lge_gee-common" remote="vanir" revision="L5" />
+  <project path="device/lge/geeb" name="xboxfanj/android_device_lge_geeb" remote="github" revision="android-4.4" />
+  <project path="device/lge/gee" name="xboxfanj/android_device_lge_gee" remote="github" revision="android-4.4" />
+  
+<!--  <project path="device/samsung/d710" name="android_device_samsung_d710" remote="cm" revision="cm-12.0" />  d710 -->
+  <project path="device/samsung/codinalte" name="device_samsung_codinalte" remote="vanir" revision="L5" groups="ste" />
+  <project path="device/samsung/golden" name="device_samsung_golden" remote="vanir" revision="L5" />
+  <project path="device/samsung/n7100" name="android_device_samsung_n7100" remote="cm" revision="cm-12.0" />
+  <project path="device/samsung/skomer" name="Team-Exhibit/android_device_samsung_skomer" remote="github" revision="kk44" groups="ste" />
+
+</manifest>
